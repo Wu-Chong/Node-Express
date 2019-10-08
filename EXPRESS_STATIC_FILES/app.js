be2beb89ca4b9344e751812496f80b6fef7706b3
Wu-Chong <wu.chong.lin@iisgalvanimi.edu.it>
2019-10-08 11:51:31 +0000

    Ma2

--- EXPRESS_STATIC_FILES/app.js
+++ EXPRESS_STATIC_FILES/app.js
@@ -1,11 +1,22 @@
 const path = require('path');
 
 var express = require('express');
-var app = express(); 
+var app = express();
 app.get('/',function(req,res){
  res.sendFile(path.join(__dirname+'/index.html')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/about',function(req,res){
+ res.sendFile(path.join(__dirname+'/about.html')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/sitemap',function(req,res){
+ res.sendFile(path.join(__dirname+'/sitemap.html')); //__dirname : Ritorna la cartella del progetto
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+
+//EXPRESS_STATIC_FILES
